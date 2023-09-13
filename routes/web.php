ec8cd6d41dc592408e531261bdfab04d067f852d
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-09-13 19:18:51 +0300

    refactor web.php file

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,17 +18,12 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
-Route::get('/home', function () {
-    return view('index');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Auth::routes(['register', 'false']);
 
 require __DIR__.'/auth.php';
 
