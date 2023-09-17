e747164e7b839c0460016a06cbe1f4d6b9f8f4a4
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-09-17 16:11:21 +0300

    handle InvoiceController

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,7 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Invoice;
+use App\Models\InvoicesAttachments;
+use App\Models\InvoicesDetails;
+use App\Models\Section;
+use Exception;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class InvoiceController extends Controller
 {
@@ -12,7 +20,8 @@ class InvoiceController extends Controller
      */
     public function index()
     {
-        return view('invoices.invoices');
+        $invoices = Invoice::orderByDesc('id')->get();
+        return view('invoices.invoices', compact('invoices'));
     }
 
     /**
@@ -20,7 +29,8 @@ class InvoiceController extends Controller
      */
     public function create()
     {
-        //
+        $sections = Section::pluck('id', 'section_name');
+        return view('invoices.add_invoices', compact('sections'));
     }
 
     /**
@@ -28,7 +38,81 @@ class InvoiceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'invoice_number' => 'required|string|max:255',
+            'invoice_date' => 'required|date',
+            'due_date' => 'required|date|after_or_equal:invoice_date',
+            'product' => 'required|string|max:255',
+            'section' => 'required',
+            'amount_collection' => 'required|numeric|min:0',
+            'amount_commission' => 'required|numeric|min:0',
+            'discount' => 'required|numeric|min:0',
+            'value_vat' => 'required|numeric|min:0',
+            'rate_vat' => 'required|min:0',
+            'total' => 'required|numeric|min:0',
+            'note' => 'nullable|string|max:1000',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error($validator->errors()->first());
+        }
+
+        try {
+            Invoice::create([
+                'invoice_number' => $request->invoice_number,
+                'invoice_date' => $request->invoice_date,
+                'due_date' => $request->due_date,
+                'product' => $request->product,
+                'section_id' => $request->section,
+                'amount_collection' => $request->amount_collection,
+                'amount_commission' => $request->amount_commission,
+                'discount' => $request->discount,
+                'value_vat' => $request->value_vat,
+                'rate_vat' => $request->rate_vat,
+                'total' => $request->total,
+                'status' => 'غير مدفوعة',
+                'value_status' => 0,
+                'note' => $request->note,
+
+            ]);
+
+            $invoice_id = Invoice::latest()->first()->id;
+            InvoicesDetails::create([
+                'invoice_id' => $invoice_id,
+                'invoice_number' => $request->invoice_number,
+                'product' => $request->product,
+                'section' => $request->section,
+                'status' => 'غير مدفوعة',
+                'value_status' => 0,
+                'note' => $request->note,
+                'user' => (Auth::user()->name),
+            ]);
+
+            if ($request->hasFile('pic')) {
+
+                $invoice_id = Invoice::latest()->first()->id;
+                $image = $request->file('pic');
+                $file_name = $image->getClientOriginalName();
+                $invoice_number = $request->invoice_number;
+
+                $attachments = new InvoicesAttachments();
+                $attachments->file_name = $file_name;
+                $attachments->invoice_number = $invoice_number;
+                $attachments->created_by = Auth::user()->name;
+                $attachments->invoice_id = $invoice_id;
+                $attachments->save();
+
+                // move pic
+                $imageName = $request->pic->getClientOriginalName();
+                $request->pic->move(public_path('Attachments/' . $invoice_number), $imageName);
+            }
+
+            flash('تم اضافة الفاتورة بنجاح')->success();
+            return back();
+        } catch (Exception $e) {
+            return $this->error();
+        }
+
     }
 
     /**
@@ -42,24 +126,89 @@ class InvoiceController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Invoice $invoice)
+    public function edit($id)
     {
-        //
+        $invoice = Invoice::where('id', $id)->first();
+        $sections = Section::pluck('id', 'section_name');
+        return view('invoices.edit_invoice', compact('sections', 'invoice'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Invoice $invoice)
+    public function update(Request $request , $id)
     {
-        //
+
+        $validator = Validator::make($request->all(), [
+            'invoice_number' => 'required|string|max:255',
+            'invoice_date' => 'required|date',
+            'due_date' => 'required|date|after_or_equal:invoice_date',
+            'product' => 'required|string|max:255',
+            'section' => 'required',
+            'amount_collection' => 'required|numeric|min:0',
+            'amount_commission' => 'required|numeric|min:0',
+            'discount' => 'required|numeric|min:0',
+            'value_vat' => 'required|numeric|min:0',
+            'rate_vat' => 'required|min:0',
+            'total' => 'required|numeric|min:0',
+            'note' => 'nullable|string|max:1000',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error($validator->errors()->first());
+        }
+
+        try {
+            $invoice = Invoice::findOrFail($id);
+            $invoice->update([
+                'invoice_number' => $request->invoice_number,
+                'invoice_date' => $request->invoice_date,
+                'due_date' => $request->due_date,
+                'product' => $request->product,
+                'section_id' => $request->section,
+                'amount_collection' => $request->amount_collection,
+                'amount_commission' => $request->amount_commission,
+                'discount' => $request->discount,
+                'value_vat' => $request->value_vat,
+                'rate_vat' => $request->rate_vat,
+                'total' => $request->total,
+                'note' => $request->note,
+            ]);
+
+            flash('تم تحديث الفاتورة بنجاح')->success();
+            return back();
+        } catch (Exception $e) {
+            return $this->error();
+        }
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Invoice $invoice)
+    public function destroy(Request $request)
     {
-        //
+
+        try {
+            $invoice = Invoice::find($request->invoice_id);
+            $invoice->delete();
+            flash('تم حذف الفاتورة بنجاح')->success();
+            return back();
+        } catch (Exception) {
+            return $this->error();
+        }
+
+    }
+
+    public function getProducts($id)
+    {
+        $products = DB::table('products')->where('section_id', $id)->pluck('product_name', 'id');
+        return json_encode($products);
+    }
+
+    public function error($message = null): RedirectResponse
+    {
+        flash(translate($message ?? 'messages.Wrong'))->error();
+        return back();
     }
 }
