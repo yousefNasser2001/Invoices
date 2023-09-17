1da193d4cb96f13a8c3a1612aaddd6132c648cfb
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-09-17 16:16:53 +0300

    handle routes

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\InvoicesAttachmentsController;
+use App\Http\Controllers\InvoicesDetailsController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SectionController;
 use Illuminate\Support\Facades\Route;
@@ -17,12 +19,18 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
- Route::prefix('dashboard/admin/')->group(static function () {
+Route::prefix('dashboard/admin/')->group(static function () {
     Route::middleware(['auth', 'verified'])->group(static function () {
         Route::get('/', [AdminController::class, 'index'])->name('dashboard');
 
-        Route::resource('invoices' , InvoiceController::class);
-        Route::resource('sections' , SectionController::class);
-        Route::resource('products' , ProductController::class);
+        Route::resource('invoices', InvoiceController::class);
+        Route::resource('sections', SectionController::class);
+        Route::resource('products', ProductController::class);
+        Route::get('section/{id}', [InvoiceController::class, 'getProducts']);
+        Route::get('invoice_details/{id}', [InvoicesDetailsController::class, 'edit'])->name('invoice_details.edit');
+        Route::get('/invoice_details/{invoiceNumber}/view-file/{attachmentId}', [InvoicesDetailsController::class, 'view_file'])->name('invoice_details.view-file');
+        Route::get('/invoice_details/{invoiceNumber}/download-file/{file_name}', [InvoicesDetailsController::class, 'download_file'])->name('invoice_details.download-file');
+        Route::post('delete_file', [InvoicesDetailsController::class, 'destroy'])->name('invoice_details.delete_file');
+        Route::resource('attachments', InvoicesAttachmentsController::class);
     });
 });
