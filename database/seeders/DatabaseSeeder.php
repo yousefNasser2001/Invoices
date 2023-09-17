98e28f201b9dc9132d53b0beadf6b3110593f207
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-09-17 16:38:53 +0300

    refactor DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             SectionSeeder::class,
             ProductSeeder::class,
+            InvoiceSeeder::class,
         ]);
     }
 }
