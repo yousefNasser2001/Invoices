d0d7513bf41245a703cd9b0a0ff077fec6b0781b
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-09-18 02:24:31 +0300

    include libraries for sweet alert and toastr

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,32 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
-		<meta name="Author" content="Spruko Technologies Private Limited">
-		<meta name="Keywords" content="admin,admin dashboard,admin dashboard template,admin panel template,admin template,admin theme,bootstrap 4 admin template,bootstrap 4 dashboard,bootstrap admin,bootstrap admin dashboard,bootstrap admin panel,bootstrap admin template,bootstrap admin theme,bootstrap dashboard,bootstrap form template,bootstrap panel,bootstrap ui kit,dashboard bootstrap 4,dashboard design,dashboard html,dashboard template,dashboard ui kit,envato templates,flat ui,html,html and css templates,html dashboard template,html5,jquery html,premium,premium quality,sidebar bootstrap 4,template admin bootstrap 4"/>
-		@include('layouts.head')
-	</head>
 
-	<body class="main-body app sidebar-mini">
-		<!-- Loader -->
-		<div id="global-loader">
-			<img src="{{URL::asset('assets/img/loader.svg')}}" class="loader-img" alt="Loader">
-		</div>
-		<!-- /Loader -->
-		@include('layouts.main-sidebar')		
-		<!-- main-content -->
-		<div class="main-content app-content">
-			@include('layouts.main-header')			
-			<!-- container -->
-			<div class="container-fluid">
-				@yield('page-header')
-				@yield('content')
-				@include('layouts.sidebar')
-				@include('layouts.models')
-            	@include('layouts.footer')
-				@include('layouts.footer-scripts')	
-	</body>
+<head>
+    <meta charset="UTF-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
+    <meta name="Author" content="Spruko Technologies Private Limited">
+    <meta name="Keywords"
+        content="admin,admin dashboard,admin dashboard template,admin panel template,admin template,admin theme,bootstrap 4 admin template,bootstrap 4 dashboard,bootstrap admin,bootstrap admin dashboard,bootstrap admin panel,bootstrap admin template,bootstrap admin theme,bootstrap dashboard,bootstrap form template,bootstrap panel,bootstrap ui kit,dashboard bootstrap 4,dashboard design,dashboard html,dashboard template,dashboard ui kit,envato templates,flat ui,html,html and css templates,html dashboard template,html5,jquery html,premium,premium quality,sidebar bootstrap 4,template admin bootstrap 4" />
+    @include('layouts.head')
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.all.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
+
+</head>
+
+<body class="main-body app sidebar-mini">
+    <!-- Loader -->
+    <div id="global-loader">
+        <img src="{{ URL::asset('assets/img/loader.svg') }}" class="loader-img" alt="Loader">
+    </div>
+    <!-- /Loader -->
+    @include('layouts.main-sidebar')
+    <!-- main-content -->
+    <div class="main-content app-content">
+        @include('layouts.main-header')
+        <!-- container -->
+        <div class="container-fluid">
+            @yield('page-header')
+            @yield('content')
+            @include('layouts.sidebar')
+            @include('layouts.models')
+            @include('layouts.footer')
+            @include('layouts.footer-scripts')
+</body>
+
 </html>
