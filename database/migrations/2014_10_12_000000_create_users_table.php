4f266aa7068d6624074cde18235024a41d4ee190
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-09-30 23:31:20 +0300

    add start and end work hour to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->time('working_hours_start')->nullable();
+            $table->time('working_hours_end')->nullable();
             $table->text('roles_name');
             $table->string('status' , 10);
             $table->rememberToken();
