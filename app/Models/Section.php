22aa12a8ba57e910d787b9ac79edee8106d8b36d
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-10-04 14:26:07 +0300

    handle Activity Classes in Section Model

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -4,12 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Section extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
 
     protected $fillable = ['section_name', 'description', 'created_by'];
+
+    protected static $logAttributes = [
+        'section_name' => 'اسم القسم',
+        'description' => 'الوصف',
+        'created_by' => 'تم الإنشاء بواسطة',
+    ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['section_name', 'description', 'created_by'])
+            ->logOnlyDirty()
+            ->setDescriptionForEvent(function (string $eventName) {
+                switch ($eventName) {
+                    case 'created':
+                        return 'انشاء قسم جديد';
+                    case 'updated':
+                        return 'تحديث بيانات قسم';
+                    case 'deleted':
+                        return 'حذف قسم';
+                    default:
+                        return "This model has been {$eventName}";
+                }
+            })
+            ->useLogName('Section');
+
+    }
 
     public function products()
     {
